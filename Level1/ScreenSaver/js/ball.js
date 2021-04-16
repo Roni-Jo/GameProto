66c259cdc390aa02b84b46b7efcf9f1bc63ecc09
Roni-Jo <rlmorrison@email.neit.edu>
2021-04-16 10:02:58 -0400

    ball

--- Level1/ScreenSaver/js/ball.js
+++ Level1/ScreenSaver/js/ball.js
@@ -5,4 +5,22 @@ var timer
 var interval = 1000/60 //fps
 var ball = new Ball()
 
+//defaults
+this.x =
+        
+//colors
+this.color = "teal";
+
 //draw ball
+this.drawCricle = function()
+{
+    context.save();
+    context.fillStyle = this.Color;
+    context.beginPath();
+    context.translate(this.XMLDocument, this.y);
+    context.arc(0, 0, this.with/2, 0, 360 *Math.PI/180, true);
+    context.closePath();
+    context.fill();
+    context.restore();
+
+}
