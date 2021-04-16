edb5d27610318886d7dc0c9f9cb858024dc1c566
Roni-Jo <rlmorrison@email.neit.edu>
2021-04-16 11:01:52 -0400

    ball move

---
+++ Level1/Move/js/ball.js
@@ -0,0 +1,47 @@
+//variables
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var timer = setInterval(animate,1000/60)
+var ball = new GameObject()
+
+function GameObject(x,y,w,h,color)
+{
+    //defaults
+    this.x = canvas.width/2;
+    this.y = canvas.height/2;
+    this.vx = 0;
+    this.vy = 0;
+    this.width = 100;
+    this.height = 100;
+    
+            
+    //color
+    this.color = "#f0ff00";
+
+    //draw ball
+    this.drawCircle = function()
+	{
+		context.save();
+			context.fillStyle = this.color;
+			context.beginPath();
+			context.translate(this.x, this.y);
+			context.arc(0, 0, this.width/2, 0, 360 *Math.PI/180, true);
+			context.closePath();
+			context.fill();
+		context.restore();
+		
+    }	
+}
+
+this.move = function()
+	{
+		this.x += this.vx;
+		this.y += this.vy;
+    }
+    
+function animate(){
+    context.clearRect(0,0,canvas.width,canvas.height);
+    ball.x += 2;
+    ball.drawCircle();
+    
+}
